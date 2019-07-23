e86076935ba5a2fe2386aadbd95c321f70a3c48c
PedroDiniz <pedro_nork@hotmail.com>
2019-07-23 17:32:54 -0300

    fix-infos-user

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,5 +1,6 @@
 import * as Yup from 'yup';
 import User from '../models/User';
+import File from '../models/File';
 
 class UserController {
   async store(req, res) {
@@ -53,7 +54,9 @@ class UserController {
     }
 
     const { email, oldPassword } = req.body;
+
     const user = await User.findByPk(req.userId);
+
     if (email !== user.email) {
       const userExists = await User.findOne({
         where: { email },
@@ -68,13 +71,23 @@ class UserController {
       return res.status(401).json({ error: 'Passwords does not match' });
     }
 
-    const { id, name, provider } = await user.update(req.body);
+    await user.update(req.body);
+
+    const { id, name, avatar } = await User.findByPk(req.userId, {
+      incluide: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     return res.json({
       id,
+      avatar,
       name,
       email,
-      provider,
     });
   }
 }
